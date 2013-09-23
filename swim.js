bcd4ba57831c394836356515b454270f4e5cf180
ayancruz <ayan.cruz@jpi.com.ph>
2013-09-23 16:54:23 +0800

    Version 1: Uses setInterval

---
+++ swim.js
@@ -0,0 +1,31 @@
+(function($){
+	  $.fn.swim = function(id_left, id_right) {
+			var that = this;
+			
+			setInterval(function() {
+			var $face_left = that.add('#' + id_left);
+			var $face_right = that.add('#' + id_right);
+			
+			var pos_top = Math.random() * 1000;
+			var pos_left = Math.random() * 1000;
+			
+			/* Left Face */			
+			if (that.position().left > pos_left) {			
+			$face_right.hide();
+			$face_left.show().addClass('#' + id_left);
+			}
+			
+			/* Right Face */
+			if (that.position().left < pos_left) {		
+			$face_left.hide();
+			$face_right.show().addClass('#' + id_right);
+			}
+			
+			/* Moves element */
+			that.stop().animate({
+					top:  pos_top + 'px',
+					left: pos_left + 'px'
+				}, 3000);
+			}, 3000);
+		}
+})(jQuery);
